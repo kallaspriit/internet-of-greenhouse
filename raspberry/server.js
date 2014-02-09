b2dee7fb280e0a86290550c60716b82602a44e18
Priit Kallas <kallaspriit@gmail.com>
2014-02-09 04:33:51 +0200

    Updated irrigation

--- raspberry/server.js
+++ raspberry/server.js
@@ -293,9 +293,9 @@ function tick(dt, currentTime) {
 		if (lastStatus === null || lastStatus[name] !== status[name]) {
 			value = status[name];
 
-			if (name === 'oxygen' && value === 1) {
+			if (name === 'irrigation' && value !== 0) {
 				value = 128;
-			} else if (name === 'lighting' && value === 1) {
+			} else if (name === 'lighting' && value !== 0) {
 				value = 255;
 			}
 
